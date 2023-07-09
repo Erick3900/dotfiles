859343aa74a6a51f31e7cc17245de00f7238e9ef
Erick3900 <erick.guzmanra@anahuac.mx>
2023-07-08 19:21:11 -0600

    Modified arti/env.sh

--- arti/env.sh
+++ arti/env.sh
@@ -7,3 +7,7 @@ fi
 if [ -d "$( dirname -- $0; )/sub/bin" ]; then
     export PATH="$PATH:$( dirname -- $0; )/sub/bin"
 fi
+
+if [ -d "$( dirname -- $0; )/spotify/bin" ]; then
+    export PATH="$PATH:$( dirname -- $0; )/spotify/bin"
+fi
